ac04cdc9541f867b747bb1b3cdb4830cdfc83c8c
Rishabh Khanna <rishabhrrk@gmail.com>
2020-03-20 14:01:50 -0400

    Updates

---
+++ precache-manifest.c53ebf4a708654c3eb894199dfdaa3a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c492456bf715fad11bbc565bc33b148",
+    "url": "/nrate/index.html"
+  },
+  {
+    "revision": "5687d5a99db1b7146594",
+    "url": "/nrate/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "362cb96b4e4536b5e9de",
+    "url": "/nrate/static/css/main.64b4ea4a.chunk.css"
+  },
+  {
+    "revision": "5687d5a99db1b7146594",
+    "url": "/nrate/static/js/2.eba6c4fd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nrate/static/js/2.eba6c4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362cb96b4e4536b5e9de",
+    "url": "/nrate/static/js/main.9d6bd470.chunk.js"
+  },
+  {
+    "revision": "402690db0eadb5a8a0e5",
+    "url": "/nrate/static/js/runtime-main.3071055d.js"
+  }
+]);
